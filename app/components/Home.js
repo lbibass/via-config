206d4c72e5342b7f84798f90519794aa8e65beb5
Olivia Briggs <ofbriggs@gmail.com>
2018-04-16 23:50:57 -0400

    Configure UI to show plugged in devices

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,18 +2,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+const HID = require('node-hid');
 
 type Props = {};
 
+function scanDevices() {
+  return HID.devices();
+}
+
 export default class Home extends Component<Props> {
   props: Props;
+
+  constructor() {
+    super();
+    this.state = {devices: scanDevices()};
+  }
+
+  updateDevices() {
+    this.setState({devices: scanDevices()});
+  }
 
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <h2>Devices:</h2>
+          <button onClick={this.updateDevices.bind(this)}>Update Devices</button>
+          <div>{this.state.devices.map(({product}) => <div>{product}</div>)}</div>
         </div>
       </div>
     );
