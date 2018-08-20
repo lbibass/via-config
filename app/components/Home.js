8495b382c4afb81f1a2aa6fd8f4301bd5d725135
Olivia Briggs <ofbriggs@gmail.com>
2018-08-19 23:08:44 -0400

    Print all possible qmk codes

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,11 @@
 // @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import styles from './Home.css';
-import { Key } from './Key';
-import { CenterKey } from './CenterKey';
-import { parseKLERaw } from '../utils/kle-parser';
-import { isAlpha, isNumericSymbol } from '../utils/key';
+import {Key} from './Key';
+import {CenterKey} from './CenterKey';
+import {parseKLERaw} from '../utils/kle-parser';
+import {getKeycodes, isAlpha, isNumericSymbol} from '../utils/key';
 const HID = require('node-hid');
 
 type Props = {};
@@ -21,30 +21,40 @@ export default class Home extends Component<Props, {}> {
 
   constructor() {
     super();
-    this.state = { devices: scanDevices() };
+    this.state = {devices: scanDevices()};
   }
 
   buildKeyboard() {
-    return <div className={styles.keyboard}>
-      {parseKLERaw().map(arr => <div className={styles.row}>{arr.map(key => this.chooseKey(key))}</div>)}
-    </div>;
+    return (
+      <div className={styles.keyboard}>
+        {parseKLERaw().map(arr => (
+          <div className={styles.row}>
+            {arr.map(key => this.chooseKey(key))}
+          </div>
+        ))}
+      </div>
+    );
   }
 
-  chooseKey({ label, size }) {
+  chooseKey({label, size}) {
     if (isAlpha(label)) {
-      return (label && <Key label={label} size={size} />);
+      return label && <Key label={label} size={size} />;
     } else if (isNumericSymbol(label)) {
       const topLabel = label[0];
       const bottomLabel = label[label.length - 1];
-      return (topLabel && bottomLabel && <Key topLabel={topLabel} bottomLabel={bottomLabel} size={size} />);
+      return (
+        topLabel &&
+        bottomLabel && (
+          <Key topLabel={topLabel} bottomLabel={bottomLabel} size={size} />
+        )
+      );
     } else {
-      return (<CenterKey label={label} size={size} />);
+      return <CenterKey label={label} size={size} />;
     }
-
   }
 
   updateDevices() {
-    this.setState({ devices: scanDevices() });
+    this.setState({devices: scanDevices()});
   }
 
   render() {
@@ -53,12 +63,23 @@ export default class Home extends Component<Props, {}> {
         <div className={styles.container} data-tid="container">
           <h2>Devices:</h2>
           <button onClick={() => this.updateDevices()}>Refresh Devices</button>
-          <select>{this.state.devices.map(({ manufacturer, product, path }) =>
-            <option key={path}>{product} ({manufacturer})</option>)}
+          <select>
+            {this.state.devices.map(({manufacturer, product, path}) => (
+              <option key={path}>
+                {product} ({manufacturer})
+              </option>
+            ))}
           </select>
         </div>
 
         {this.buildKeyboard()}
+        <div>
+          {getKeycodes().map(({code, name}) => (
+            <div>
+              {code}: {name}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
