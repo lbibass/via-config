1b7e552d603599341e32d6b656cc62bed27a6ca1
Olivia Briggs <ofbriggs@gmail.com>
2018-10-20 16:19:59 -0400

    Add pattern enum

--- app/components/lighting-categories/pattern.js
+++ app/components/lighting-categories/pattern.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
 import styles from './pattern.css';
 
+const Pattern = [
+  'All Off',
+  'Solid Color 1',
+  'Alphas/Mods Color 1/2',
+  'Gradient Vertical Color 1/2',
+  'Raindrops Color 1/2',
+  'Cycle All',
+  'Cycle Horizontal',
+  'Cycle Vertical',
+  'Jellybean Raindrops',
+  'Radial Color 1',
+  'Radial All Hues'
+];
+
 export class PatternCategory extends Component {
   onRangeChange(event) {
     const pattern = parseInt(event.target.value);
