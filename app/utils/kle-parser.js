590d98d3938b9e71f5d79712ec81d9ed752cca72
lbibass <notlbibass@gmail.com>
2019-01-16 21:57:20 -0500

    Fixed It!

--- app/utils/kle-parser.js
+++ app/utils/kle-parser.js
@@ -121,11 +121,11 @@ export const LAYOUT_STANDARD_60_HHKB = `[{c:"#4d525a",t:"#e2e2e2"},"Esc",{c:"#e2
 [{w:1.5},"Ctrl","Win",{w:1.5},"Alt",{c:"#e2e2e2",t:"#363636",a:7,w:7},"x",{c:"#4d525a",t:"#e2e2e2",a:4,w:1.5},"Alt","Win",{w:1.5},"Ctrl"]`;
 
 export const LAYOUT_BNB87_ALL = `["Esc",{x:1},"F1","F2","F3","F4",{x:0.5},"F5","F6","F7","F8",{x:0.5},"F9","F10","F11","F12",{x:0.25},"PrtSc","Scroll Lock","Pause\nBreak"],
-[{y:0.5},"~\n`","!\n1","@\n2","#\n3","$\n4","%\n5","^\n6","&\n7","*\n8","(\n9",")\n0","_\n-","+\n=",{w:2},"Backspace",{x:0.25},"Insert","Home","PgUp"],
+[{y:0.5},"~","!\n1","@\n2","#\n3","$\n4","%\n5","^\n6","&n7","*n8","(\n9",")\n0","_\n-","+\n=",{w:2},"Backspace",{x:0.25},"Insert","Home","PgUp"],
 [{w:1.5},"Tab","Q","W","E","R","T","Y","U","I","O","P","{\n[","}\n]",{w:1.5},"|\n\\",{x:0.25},"Delete","End","PgDn"],
 [{w:1.75},"Caps Lock","A","S","D","F","G","H","J","K","L",":\n;","\"\n'",{w:2.25},"Enter"],
 [{w:2.25},"Shift","Z","X","C","V","B","N","M","<\n,",">\n.","?\n/",{w:1.75},"Shift",{a:7},"",{x:1.25,a:4},"↑"],
-[{w:1.25},"Ctrl",{w:1.25},"Win",{w:1.25},"Alt",{a:7,w:6.25},"",{a:4,w:1.25},"Alt",{w:1.25},"Win",{w:1.25},"Menu",{w:1.25},"Ctrl",{x:0.25},"←","↓","→"]`:
+[{w:1.25},"Ctrl",{w:1.25},"Win",{w:1.25},"Alt",{a:7,w:6.25},"",{a:4,w:1.25},"Alt",{w:1.25},"Win",{w:1.25},"Menu",{w:1.25},"Ctrl",{x:0.25},"←","↓","→"]`;
 
 
 export function parseKLERaw(kle: string): ParsedKLE {
